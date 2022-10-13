cc0263a9789ad81a1565d0fd21ed249256a7979a
Rozumek29 <k.wyrozumialski@a-soft.pl>
2022-10-13 20:15:24 +0200

    Refactored img list injection.

---
+++ src/arts/artsData.js
@@ -0,0 +1,50 @@
+export const artsData = [
+  {
+    "src": "https://cdna.artstation.com/p/assets/images/images/053/966/010/large/metramin-akaliavat4ar.jpg?1663446049",
+    "title": "Akali KDA",
+    "cols": 2,
+    "rows": 3
+  },
+  {
+    "src": "https://cdna.artstation.com/p/assets/images/images/053/510/328/large/metramin-12.jpg?1662391632",
+    "title": "Twój stary szukający żabki",
+    "cols": 2,
+    "rows": 3
+  },
+  {
+    "src": "https://cdna.artstation.com/p/assets/images/images/053/328/480/large/metramin-obraz-2022-08-31-155059355.jpg?1661953858",
+    "title": "Doktor, Ziemniak, Chuj wie, Twoja stara zanim poznała starego, nemo",
+    "cols": 2,
+    "rows": 3
+  },
+  {
+    "src": "https://cdnb.artstation.com/p/assets/images/images/053/966/155/large/metramin-kolczasty-czarodziej.jpg?1663446536",
+    "title": "Jesz",
+    "cols": 2,
+    "rows": 3
+  },
+  {
+    "src": "https://cdna.artstation.com/p/assets/images/images/052/765/526/large/metramin-yellow-glowing-horn.jpg?1660628537",
+    "title": "Step sister",
+    "cols": 2,
+    "rows": 3
+  },
+  {
+    "src": "https://cdnb.artstation.com/p/assets/images/images/050/738/329/large/metramin-armor-ghost-concept.jpg?1655565754",
+    "title": "Stara po wywiadówce",
+    "cols": 2,
+    "rows": 3
+  },
+  {
+    "src": "https://cdna.artstation.com/p/assets/images/images/050/897/256/large/metramin-friendly-compaion-wulu.jpg?1655949411",
+    "title": "Wuly fury McDonalds",
+    "cols": 2,
+    "rows": 3
+  },
+  {
+    "src": "https://cdna.artstation.com/p/assets/images/images/054/722/192/large/metramin-rebunia-metramunia-xd.jpg?1665204952",
+    "title": "Jinx po farbowaniu",
+    "cols": 2,
+    "rows": 3
+  }
+]
--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -2,39 +2,19 @@ import ArtsList from "./components/ArtsList";
 import Grid from "@mui/material/Grid";
 import styled from "styled-components";
 import {useEffect, useState} from "react";
+import {artsData} from "../../arts/artsData";
 
 const PortfolioContainer = styled(Grid)`
   width: 100%;
   height: 100%;
 `;
 
-function getAllImages() {
-    const r = require.context('../../arts', false, /\.(png|jpe?g|svg)$/);
-    return r.keys().map(r);
-}
-
 const Gallery = () => {
     const [artList, setArtList] = useState([]);
 
     useEffect(() => {
         document.title = "Mateusz Pawelak | Portfolio";
-        if (artList.length < 1) {
-            let tempArtList = [];
-            let images = getAllImages();
-            images.map((image) => {
-                let tempImg = new Image();
-                tempImg.onload = () => {
-                    tempArtList.push({
-                        src: image,
-                        width: tempImg.width,
-                        height: tempImg.height,
-                    });
-                    setArtList(tempArtList);
-                }
-                tempImg.src = image;
-            })
-            setArtList(tempArtList);
-        }
+        setArtList(artsData);
     }, []);
 
     return (
